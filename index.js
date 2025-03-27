f0f630318307556a6ba49577a084d3b74237d0d3
eupremio <cinierieupremio@gmail.com>
2025-03-27 11:57:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let annoCorrente = 2025;
+let annoDiNascita = 1999;
+
+let età = annoCorrente - annoDiNascita;
+console.log(età);
+
+let anniMancanti = 100 - età 
+console.log( anniMancanti);
+
+console.log( `hai ${età} anni e mancano ${anniMancanti} anni per compierne 100` );
